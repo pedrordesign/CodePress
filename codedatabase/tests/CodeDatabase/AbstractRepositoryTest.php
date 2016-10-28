4f22770b506a85329e8ea08f23b1eddd485d3f63
Pedro Ramos <pedroramos@sitehosting.com.br>
2016-10-28 10:41:00 -0200

    Mock de repository-all

--- codedatabase/tests/CodeDatabase/AbstractRepositoryTest.php
+++ codedatabase/tests/CodeDatabase/AbstractRepositoryTest.php
@@ -10,8 +10,44 @@ class AbstractRepositoryTest extends AbstractTestCase
 {
 
     public function test_if_implements_repositoryinterface(){
+
         $mock = m::mock(AbstractRepository::class);
         $this->assertInstanceOf(RepositoryInterface::class, $mock);
+
+    }
+
+    public function test_should_return_all_without_arguments(){
+
+        $mockRepository = m::mock(AbstractRepository::class);
+        $mockStd = m::mock(\stdClass::class);
+
+        $mockStd->id = 1;
+        $mockStd->name = 'name';
+        $mockStd->description = 'description';
+
+        $mockRepository->shouldReceive('all')
+            ->andReturn([$mockStd,$mockStd,$mockStd]);
+
+        $this->assertCount(3, $mockRepository->all());
+        $this->assertInstanceOf(\stdClass::class, $mockRepository->all()[0]);
+
+    }
+
+    public function test_should_return_all_with_arguments(){
+
+        $mockRepository = m::mock(AbstractRepository::class);
+        $mockStd = m::mock(\stdClass::class);
+
+        $mockStd->id = 1;
+        $mockStd->name = 'name';
+
+        $mockRepository->shouldReceive('all')
+            ->with(['id','name'])
+            ->andReturn([$mockStd,$mockStd,$mockStd]);
+
+        $this->assertCount(3, $mockRepository->all(['id', 'name']));
+        $this->assertInstanceOf(\stdClass::class, $mockRepository->all(['id', 'name'])[0]);
+
     }
 
 }
