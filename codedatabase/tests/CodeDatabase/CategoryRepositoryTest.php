c759cc26025b57007da6b27791aea544cb65859b
Pedro Ramos <pedroramos@sitehosting.com.br>
2016-10-28 15:09:32 -0200

    Adicionando categorias padrões no teste

--- codedatabase/tests/CodeDatabase/CategoryRepositoryTest.php
+++ codedatabase/tests/CodeDatabase/CategoryRepositoryTest.php
@@ -10,42 +10,59 @@ use Mockery as m;
 
 class CategoryRepositoryTest extends AbstractTestCase
 {
+    private $repository;
     public function setUp()
     {
         parent::setUp();
         $this->migrate();
+        $this->repository = new CategoryRepository();
+        $this->createCategory();
     }
 
     public function test_can_model()
     {
-        $repository = new CategoryRepository();
-        $this->assertEquals(Category::class, $repository->model());
+        $this->assertEquals(Category::class, $this->repository->model());
     }
 
     public function test_if_can_make_model()
     {
-        $repository = new CategoryRepository();
-        $result = $repository->makeModel();
+        $result = $this->repository->makeModel();
         $this->assertInstanceOf(Category::class, $result);
 
-        $reflectionClass = new \ReflectionClass($repository);
+        $reflectionClass = new \ReflectionClass($this->repository);
         $reflectionProperty = $reflectionClass->getProperty('model');
         $reflectionProperty->setAccessible(true);
 
-        $result = $reflectionProperty ->getValue($repository);
+        $result = $reflectionProperty ->getValue($this->repository);
         $this->assertInstanceOf(Category::class, $result);
     }
 
     public function test_can_make_model_in_constructor()
     {
-        $repository = new CategoryRepository();
 
-        $reflectionClass = new \ReflectionClass($repository);
+        $reflectionClass = new \ReflectionClass($this->repository);
         $reflectionProperty = $reflectionClass->getProperty('model');
         $reflectionProperty->setAccessible(true);
 
-        $result = $reflectionProperty ->getValue($repository);
+        $result = $reflectionProperty ->getValue($this->repository);
         $this->assertInstanceOf(Category::class, $result);
+    }
+
+    public function createCategory()
+    {
+        Category::create([
+            'name' => 'Category 1',
+            'description' => 'Description 1'
+        ]);
+        Category::create([
+            'name' => 'Category 2',
+            'description' => 'Description 2'
+        ]);
+        Category::create([
+            'name' => 'Category 3',
+            'description' => 'Description 3'
+        ]);
+
     }
 
 }
