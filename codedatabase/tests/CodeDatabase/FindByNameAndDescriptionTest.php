263133930b7765fbf2515430fd7f3529e3ce105c
Pedro Ramos <pedroramos@sitehosting.com.br>
2016-10-31 14:43:23 -0200

    Testando Stub Criteria

--- codedatabase/tests/CodeDatabase/FindByNameAndDescriptionTest.php
+++ codedatabase/tests/CodeDatabase/FindByNameAndDescriptionTest.php
@@ -6,6 +6,7 @@ use CodePress\CodeDatabase\Criteria\FindByNameAndDescription;
 use CodePress\CodeDatabase\Models\Category;
 use CodePress\CodeDatabase\Repository\CategoryRepository;
 use CodePress\CodeDatabase\Contracts\CriteriaInterface;
+use Illuminate\Database\Eloquent\Builder;
 use Mockery as m;
 
 class FindByNameAndDescriptionTest extends AbstractTestCase
@@ -22,13 +23,31 @@ class FindByNameAndDescriptionTest extends AbstractTestCase
         parent::setUp();
         $this->migrate();
         $this->repository = new CategoryRepository();
-        $this->criteria = new FindByNameAndDescription();
+        $this->criteria = new FindByNameAndDescription('Category 1', 'Description 1');
         $this->createCategory();
     }
 
     public function test_if_is_instanceof_criteria_interface()
     {
         $this->assertInstanceOf(CriteriaInterface::class, $this->criteria);
+    }
+
+    /**
+     * @var FindByNameAndDescription
+     */
+    public function test_if_apply_returns_query_builder()
+    {
+        $class = $this->repository->model();
+        $result = $this->criteria->apply(new $class, $this->repository);
+        $this->assertInstanceOf(Builder::class, $result);
+    }
+
+    public function test_if_apply_returns_data()
+    {
+        $class = $this->repository->model();
+        $result = $this->criteria->apply(new $class, $this->repository)->get()->first();
+        $this->assertEquals('Category 1', $result->name);
+        $this->assertEquals('Description 1', $result->description);
     }
 
     public function createCategory()
